9b925976024d4031a53b942b8663c24c49c7a820
Dhrumil1808 <dhrml.shah@gmail.com>
2017-06-21 01:06:18 -0700

    node value linked list

---
+++ Linked_list/node_value_Linkedlist.java
@@ -0,0 +1,33 @@
+/*
+  Get Nth element from the end in a linked list of integers
+  Number of elements in the list will always be greater than N.
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    
+int GetNode(Node head,int n) {
+     // This is a "method-only" submission. 
+     // You only need to complete this method. 
+
+    Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+       head = prev;
+        int counter=0;
+    while(counter!=n)
+        {
+        head=head.next;
+        counter++;
+    }
+    return head.data;
+
+}
